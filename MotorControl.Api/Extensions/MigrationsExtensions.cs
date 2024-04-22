8a492a2bf3b076091aa7b577c3dfb7540abe07f7
David Caballero Savon <caballero.david2011@gmail.com>
2024-04-21 22:58:28 -0300

    commit

---
+++ MotorControl.Api/Extensions/MigrationsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotorControl.Api.Repository.Context;
+
+namespace MotorControl.Api.Extensions
+{
+    public static class MigrationsExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+            using MotorControlDbContext dbContext = scope.ServiceProvider.GetRequiredService<MotorControlDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
