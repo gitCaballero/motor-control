aa227fafc242e97ee6d2f0a4139f4954f36b3a80
David Caballero Savon <caballero.david2011@gmail.com>
2024-04-22 14:10:07 -0300

    v1

---
+++ MotorControl.Api/Migrations/20240422144314_Initials.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MotorControl.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "motors",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    Year = table.Column<string>(type: "text", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    Plate = table.Column<string>(type: "text", nullable: false),
+                    IsAvalable = table.Column<int>(type: "integer", nullable: false),
+                    Identifier = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_motors", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "motors");
+        }
+    }
+}
